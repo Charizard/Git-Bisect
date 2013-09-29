17174e970cb28d4d954e3594bf837be9047933b4
Yuvaraja <yuv.slm@gmail.com>
2013-09-29 19:33:27 +0530

    additional specs for the addition method

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -1,7 +1,12 @@
 require 'spec_helper'
 
 describe Bisect::Math do
-  it "Math.add(5,6) should return 11" do
-    expect( Bisect::Math.add(5, 6) ).to be 11
+  context "#add" do
+    it "Math.add(5,6) should return 11" do
+      expect( Bisect::Math.add(5, 6) ).to be 11
+    end
+    it "Math.add(5, -10) should return -5" do
+      expect( Bisect::Math.add(5, -10) ).to be -5
+    end
   end
 end
